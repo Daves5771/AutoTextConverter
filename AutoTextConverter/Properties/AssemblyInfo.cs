b9f5faf276337d16d34db5f1096167e6bc604cb8
David Saelman <daves5771@gmail.com>
2016-03-14 11:42:27 +0200

    version 0.81000

---
+++ AutoTextConverter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AutoTextConverter - Beta")]
+[assembly: AssemblyDescription("Application to perform complex search and replacements on MS WORD documents via regular expressions.\nFor more information contact - Daves5771@gmail.com")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Saelsoft -")]
+[assembly: AssemblyProduct("AutoTextConverter")]
+[assembly: AssemblyCopyright("Copyright ©  2008-2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("02c4cced-d53e-4906-9ed9-5548f8b2185f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.8.1.0")]
+[assembly: AssemblyFileVersion("0.8.1.0")]
