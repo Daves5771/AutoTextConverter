bef97250a1bdda84c57208b87a49646e880d1941
David Saelman <daves5771@gmail.com>
2016-03-14 17:45:58 +0200

    version 0.8100 Added

---
+++ AutoTextConverter/Commands.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SaelSoft.AutoTextConverter
+{
+    public class Commands
+    {
+        public const int Process = 1;
+        public const int Step = 2;
+        public const int PausePause = 4;
+        public const int PauseResume = 8;
+        public const int Stop = 16;
+    }
+}
